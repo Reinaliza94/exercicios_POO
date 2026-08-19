2ef249512209e824ad73d7b785e6f30c0c8ce5f3
Izabel Reinaldo <reinaldo.sousa05@aluno.ifce.edu.br>
2026-08-19 14:47:58 -0300

    Add files via upload

---
+++ poo.py
@@ -0,0 +1,20 @@
+class Livro:
+    def __init__(self, titulo, autor, ano):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+
+livro = Livro ("O pequeno príncipe", "Antonine de Saint-Exupery", 1943)
+print(f"Titulo: {livro.titulo}")
+print(f"autor: {livro.autor}")
+print(f"ano: {livro.ano}")
+
+
+
+
+
+        
+      
+    
+    
+
